056df2bd827dc20ab1f20d1e66e362b4ca2ac4b8
claris_tannerp <tplauche@clarishealth.com>
2021-04-06 15:51:34 -0500

    change proc and port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 const app = express();
 TaskRouter = require('./controllers/task-controller');
 
